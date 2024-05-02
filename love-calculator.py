5002b8d1dd4d1474342ee073836b9b28ceda5c47
AsfinaAndini <128123181+AsfinaAndini@users.noreply.github.com>
2024-05-02 13:10:20 +0700

    first commit

---
+++ love-calculator.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import random
+root = Tk()
+# Defining the size, width=400, height=240
+root.geometry('400x240')
+# Title 
+root.title('Love Calculator❤️❤️')
+
+def calculate_love():
+    # value will contain digits between 0-9
+    st = '0123456789'
+    # result will be in double digits
+    digit = 2
+    temp = "".join(random.sample(st, digit))
+    result.config(text=temp)
+
